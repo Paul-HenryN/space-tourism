4109bba2983cc773e0188754fc6d9df4c2b2c1cf
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2023-05-23 10:20:10 +0100

    feat: App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
 import './styles/reset.css'
 import './styles/variables.css'
 import './styles/utils.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './routes/root'
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </React.StrictMode>,
 )
