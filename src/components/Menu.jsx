a876b30b91f4ac8cef5e67f06bc7c0a856952811
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2023-05-23 16:21:24 +0100

    feat: home route

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,14 @@
 import styles from "../styles/components/Menu.module.css";
 
-export default function Menu({className}) {
-    return (
-        <nav className={`${styles.menu} ${className}`}>
-            <ul className="flex col-gap-md" role="list">
-                <li>Home</li>
-                <li>Destination</li>
-                <li>Crew</li>
-                <li>Technology</li>
-            </ul>
-        </nav>
-    );
-} 
+export default function Menu({ className }) {
+  return (
+    <nav className={`${styles.menu} ${className}`}>
+      <ul className="flex col-gap-md" role="list">
+        <li>Home</li>
+        <li>Destination</li>
+        <li>Crew</li>
+        <li>Technology</li>
+      </ul>
+    </nav>
+  );
+}
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,3 +1,28 @@
+import Column from "../components/Layout/Column";
+import styles from "../styles/pages/Home.module.css";
+
 export default function Home() {
-    return <h1>This is the home</h1>;
+  return (
+    <Column
+      className={`${styles.container} align-center row-gap-xxl mx-auto mg-top-sm`}
+    >
+      <Column className="row-gap-xxs">
+        <Column className="row-gap-xxs">
+          <p className="heading-5">So, you want to travel to</p>
+          <h1 className="heading-1">space</h1>
+        </Column>
+
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </Column>
+
+      <a href="#" className="btn-primary">
+        Explore
+      </a>
+    </Column>
+  );
 }
