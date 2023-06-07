22596ea14e8155842d2ace2eec3f1ef162eee889
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2023-06-07 21:30:16 +0100

    fix: CarouselButton size

--- src/components/CarouselButton.jsx
+++ src/components/CarouselButton.jsx
@@ -1,6 +1,5 @@
 export default function CarouselButton({ active, onClick, children }) {
-  let finalClass =
-    "flex align-center justify-center pd-xxs pd-md-sm pd-lg-md ff-primary fs-1 txt-secondary border border-1 border-secondary square round hover-bg-secondary hover-txt-primary";
+  let finalClass = `flex align-center justify-center ff-primary fs-1 txt-secondary border border-1 border-secondary square round hover-bg-secondary hover-txt-primary`;
 
   if (active) {
     finalClass = finalClass.replace("txt-secondary", "txt-primary");
@@ -9,6 +8,7 @@ export default function CarouselButton({ active, onClick, children }) {
 
   return (
     <button
+      style={{ width: "clamp(40px, 7.8vw, 80px)" }}
       className={finalClass}
       onClick={() => {
         onClick();
