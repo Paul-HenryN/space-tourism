5e305fc0538f1d5e8f84f390064175346e3233cc
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2023-06-13 13:32:05 +0100

    chore: deploy

--- src/shared/globals.jsx
+++ src/shared/globals.jsx
@@ -3,27 +3,27 @@ import Destination from "../routes/Destination";
 import Crew from "../routes/Crew";
 import Technology from "../routes/Technology";
 
-export const dataFile = "./space-tourism/src/data.json";
+export const dataFile = "src/data.json";
 
 export const pages = [
   {
     name: "Home",
-    path: "/space-tourism",
+    path: "/space-tourism/",
     element: <Home />,
   },
   {
     name: "Destination",
-    path: "/space-tourism/destination",
+    path: "/space-tourism/destination/",
     element: <Destination />,
   },
   {
     name: "Crew",
-    path: "/space-tourism/crew",
+    path: "/space-tourism/crew/",
     element: <Crew />,
   },
   {
     name: "Technology",
-    path: "/space-tourism/technology",
+    path: "/space-tourism/technology/",
     element: <Technology />,
   },
 ];
